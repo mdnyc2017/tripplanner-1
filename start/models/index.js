e04519b9ab86bf6fbc660cde3c906f077f20eb83
Dima Reykhtman <neoxdth@gmail.com>
2017-10-03 18:13:49 -0400

    trip planner

---
+++ start/models/index.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/tripplanner', {logging: false});
+
+const Place = db.define('place', {
+    address: {
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    location: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+    }
+});
+
+const Hotel = db.define('hotel', {
+    name: {
+        type: Sequelize.STRING
+    },
+    num_stars: {
+        type: Sequelize.INTEGER
+    },
+    amenities: {
+        type: Sequelize.TEXT
+    },
+    address: {
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    location: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+    }
+});
+
+const Activity = db.define('activity', {
+    name: {
+        type: Sequelize.STRING
+    },
+    age_range: {
+        type: Sequelize.STRING
+    }
+});
+
+const Restaurant = db.define('restaurant', {
+    name: {
+        type: Sequelize.STRING
+    },
+    cuisine: {
+        type: Sequelize.TEXT
+    },
+    price: {
+        type: Sequelize.INTEGER
+    }
+});
+
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
+module.exports = {
+    db,
+    Place,
+    Hotel,
+    Activity,
+    Restaurant
+};
